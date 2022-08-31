622a4be2450197e894dd7fb9ebde64cd4096ef5c
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-31 11:36:56 -0400

    :wrench: [ADD] struture and dev env config
    Configure eslint, prettier and next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    customKey: 'customValue', //to work with environments variables
+  },
+  basePath: '/dist', //where is generate the projects
+  compress: true, //compress gzip active
+  //redirects urls
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/hi', //can be https://urls
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
