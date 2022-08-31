0c6c14928293084fe8b1b24c2e6935bff4eac122
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-31 18:08:02 -0400

    :sparkles: [ADD] pwa characteritics

--- next.config.js
+++ next.config.js
@@ -1,25 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['api.lorem.space', 'cdn.pixabay.com', 'placeimg.com', 'www.libreriahuequito.com', 'thumbs.dreamstime.com'],
   },
-  /*env: {
-    customKey: 'customValue', //to work with environments variables
-  },
-  basePath: '/dist', //where is generate the projects
-  compress: true, //compress gzip active
-  //redirects urls
-  async redirects() {
-    return [
-      {
-        source: '/hola',
-        destination: '/hi', //can be https://urls
-        permanent: true,
-      },
-    ];
-  },*/
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
